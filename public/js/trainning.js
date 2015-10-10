9f1285a283f5995d2c524402ef6c17841241cf42
JesseHsiu <jesse801122@gmail.com>
2015-10-10 22:23:06 +0800

    build and clear data btn added

--- public/js/trainning.js
+++ public/js/trainning.js
@@ -94,4 +94,36 @@ function startTrainning () {
     }    
 }
 
+function buildModel () {
+    $.ajax({
+        url: 'http://127.0.0.1:3000/trainning/buildmodel',
+        dataType: "jsonp",
+        jsonpCallback: "_testcb",
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+            
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('error ' + textStatus + " " + errorThrown);
+        }
+    });
+}
+
+function clearData () {
+    $.ajax({
+        url: 'http://127.0.0.1:3000/trainning/reset',
+        dataType: "jsonp",
+        jsonpCallback: "_testcb",
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+            
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert('error ' + textStatus + " " + errorThrown);
+        }
+    });
+}
+
 
